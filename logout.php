37ea499eb047cb37414505b7c503db8aa6bb0ad0
jcmanolong21 <44686119+jcmanolong21@users.noreply.github.com>
2018-11-02 08:23:36 +0800

    lmvillamor

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@
 session_start();
 if(session_destroy()) // Destroying All Sessions
 {
-header("Location: index.html"); // Redirecting To Home Page
+header("Location: index.php"); // Redirecting To Home Page
 }
 
 ?>
